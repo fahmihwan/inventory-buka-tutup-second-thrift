720947d7dc28fc29299df442b8d34f6b9468b85c
fahmi ichwanurrohman <you@example.com>
2023-05-30 04:18:40 +0700

    revisi-1

---
+++ resources/views/pages/supplier_customer/customer/print_customer.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>.</title>
+
+    <style type="text/css">
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        .header {
+            padding: 20px 0px;
+            text-align: center;
+        }
+
+        .header h4 {
+            font-size: 28px;
+        }
+
+        .container {
+            /* border: 1px solid black; */
+        }
+
+        table {
+            width: 100%;
+        }
+
+        table,
+        th,
+        td {
+            padding: 5px;
+            border: 1px solid black;
+            border-collapse: collapse;
+            font-size: 9pt;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="header">
+        <h4>Report Customer </h4> <br>
+        <h6 style="font-size: 12px;">Buka Tutup Second</h6>
+        <p style="font-size: 12px">Jl Godean, Km 7 Semarangan, Sleman Daerah Istimewa Yogyakarta Kode Pos 55285</p>
+        <p style="margin: 5px">Periode : {{ request('start_date') }} sampai {{ request('end_date') }}</p>
+    </div>
+    <div class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Alamat</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($datas as $data)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $data->nama }}</td>
+                        <td>{{ $data->alamat }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+
+
+
+        </table>
+    </div>
+
+</body>
+
+</html>
